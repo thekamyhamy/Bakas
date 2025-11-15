e461df65e7806bad8448ebd6201c9a0d08bdba21
Tarunvir <gtarunvir@gmail.com>
2025-11-15 10:47:12 -0800

    movement

--- Assets/Character/movements.cs
+++ Assets/Character/movements.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class movements : MonoBehaviour
 {
+    [SerializeField] private float movementSpeed = 2f;
+    private Rigidbody2D rb;
+    private Vector2 movementDirection;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized; // Normalize for consistent speed in all directions
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movementDirection * movementSpeed; // Corrected: Use velocity for linear movement
     }
 }
